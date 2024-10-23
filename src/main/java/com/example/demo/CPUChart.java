cae55a40a7a8ee1a070682b9af317508ad5db65a
Yasmin Woodlock <119609274+Yasmin2k3@users.noreply.github.com>
2024-10-23 01:54:55 +0100

    Making CPU Info line chart display

---
+++ src/main/java/com/example/demo/CPUChart.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CPUChart {
+    //gets an arraylist of values every second for cpu busy percentage
+    private static void buildBusyLineChart(int busyPercent){
+
+        //temporary timer
+        int count=0;
+
+        //will be replaced with whatever the getBusyInfo will return
+        int temp = 0;
+
+
+        //list that will display CPU Busy percent as a line chart
+        List<Integer> cpuBusyPercent = new ArrayList<Integer>(5);
+
+        //will constantly update linechart
+
+        while(count < 10){
+            temp = (int)(Math.random()*100 + 1);
+            System.out.println(cpuBusyPercent);
+            if (cpuBusyPercent.size() == 5){
+                cpuBusyPercent.removeFirst();
+            }
+            cpuBusyPercent.add(temp);
+            count++;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        buildBusyLineChart(1);
+
+    }
+
+}
